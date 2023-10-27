b096ebeb056023f6623576f5b801597c4d19ab10
bora-sen <bahattinborasen@gmail.com>
2023-10-27 21:26:58 +0300

    +updated outDir

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: "build",
   integrations: [tailwind()],
   site:"https://bora-sen.github.io",
   base:"/cyberera-website"
